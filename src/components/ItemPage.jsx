521b18dadcd219d34e1ef0154c77b9cef7f36a66
Caylin Vu <caylinvu@gmail.com>
2023-09-11 18:24:23 -0500

    Add ability to change item quantity with input field

--- src/components/ItemPage.jsx
+++ src/components/ItemPage.jsx
@@ -1,9 +1,15 @@
 import { useParams, useOutletContext } from 'react-router-dom';
+import { useState } from 'react';
 import '../styles/ItemPage.css';
 
 function ItemPage() {
+  const [quantity, setQuantity] = useState(1);
   const { items } = useOutletContext();
   const { item } = useParams();
+
+  const handleInput = (e) => {
+    setQuantity(e.target.value);
+  };
 
   return (
     <div className="item-page">
@@ -18,8 +24,16 @@ function ItemPage() {
                 <div className="main-info">
                   <h2>{obj.title}</h2>
                   <p>${obj.price.toFixed(2)}</p>
-                  <div className="quantity">quantity adjuster</div>
-                  <button>Add to Cart</button>
+                  <div className="quantity">
+                    <button className="increment-btn">
+                      <img src="/minus.svg" alt="" />
+                    </button>
+                    <input type="number" value={quantity} onChange={handleInput} />
+                    <button className="increment-btn">
+                      <img src="/plus.svg" alt="" />
+                    </button>
+                  </div>
+                  <button className="add-btn">Add to Cart</button>
                 </div>
               </div>
               <div className="item-bottom">
